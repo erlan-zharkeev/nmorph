9036fb50cd9d8a55283684eb00804bf7afbb338f
Erlan Zharkeev <erlan.zharkeev@gmail.com>
2024-12-24 16:16:11 +0500

    development

--- lib-docs/plugins/nmorph.ts
+++ lib-docs/plugins/nmorph.ts
@@ -2,5 +2,9 @@ import { defineNuxtPlugin } from "#app";
 import { NmorphLibrary } from "@nmorph/nmorph-ui-kit";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  if (process.client) nuxtApp.vueApp.use(NmorphLibrary, {});
+  if (process.client) nuxtApp.vueApp.use(NmorphLibrary, {
+    i18n: {
+      outsideMessagesMerge: true
+    }
+  });
 });
