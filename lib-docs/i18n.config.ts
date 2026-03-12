058075e771742336baecf949dd8bd5b1e0c0ec28
Erlan Zharkeev <erlan.zharkeev@gmail.com>
2026-03-12 21:14:40 +0700

    fix(docs): restore nuxt i18n locale loading

--- lib-docs/i18n.config.ts
+++ lib-docs/i18n.config.ts
@@ -1,8 +1,1 @@
-import { ru, zh } from "@nmorph/nmorph-ui-kit";
-
-export default defineI18nConfig(() => ({
-  messages: {
-    ru,
-    zh,
-  },
-}));
+export default defineI18nConfig(() => ({}));
