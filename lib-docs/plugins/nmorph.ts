4cfe99e145a4064834e87ed5efcb4328971c4d1f
Erlan Zharkeev <erlan.zharkeev@gmail.com>
2026-03-13 03:35:26 +0000

    Dev

--- lib-docs/plugins/nmorph.ts
+++ lib-docs/plugins/nmorph.ts
@@ -4,16 +4,15 @@ import { NmorphLibrary, ru, zh, en } from "@nmorph/nmorph-ui-kit";
 export default defineNuxtPlugin((nuxtApp) => {
   const options = {
     i18n: {
-      outsideMessagesMerge: true,
-      messages: { ru, zh, en }
+      outsideMessagesMerge: true
     }
   };
-  // // @ts-expect-error
-  // nuxtApp.$i18n?.mergeLocaleMessage?.("ru", ru);
-  // // @ts-expect-error
-  // nuxtApp.$i18n?.mergeLocaleMessage?.("zh", zh);
-  // // @ts-expect-error
-  // nuxtApp.$i18n?.mergeLocaleMessage?.("en", en);
+  // @ts-expect-error
+  nuxtApp.$i18n?.mergeLocaleMessage?.("ru", ru);
+  // @ts-expect-error
+  nuxtApp.$i18n?.mergeLocaleMessage?.("zh", zh);
+  // @ts-expect-error
+  nuxtApp.$i18n?.mergeLocaleMessage?.("en", en);
 
   if (process.client) nuxtApp.vueApp.use(NmorphLibrary, options);
 });
